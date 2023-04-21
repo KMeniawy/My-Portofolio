e57f15d6ba9c30e3bd893a2b18eeecfcafa41837
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-04-21 02:37:48 +0200

    timeline minus the layout

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
         </ul>
       </div>
       {/* about */}
-      <div className=" h-[90vh]">
+      <div className=" h-[80vh]">
         {/* TODO Animation */}
         <h1 className="text-center my-28 text-5xl mx-auto">About Me</h1>
         <div className="grid grid-flow-row-dense grid-cols-6 mx-48">
@@ -123,88 +123,51 @@ function App() {
         <h1 className="text-center my-28 text-5xl mx-auto">My Resume</h1>
         <div className="flex justify-center">
           <ol class="relative border-l border-gray-200 dark:border-gray-700">
-            <li class="mb-10 ml-6">
-              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                <svg
-                  aria-hidden="true"
-                  class="w-3 h-3 text-blue-800 dark:text-blue-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+            <li class="mb-16 ml-8">
+              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-[#fff] hover:bg-[#0000A6] transition-colors">
               </span>
-              <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Flowbite Application UI v2.0.0{" "}
+              <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 ">
+                Faculty of Science
               </h3>
               <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                Released on January 13th, 2022
+                2018 - 2021
               </time>
               <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                Get access to over 20+ pages including a dashboard layout,
-                charts, kanban board, calendar, and pre-order E-commerce &
-                Marketing pages.
+                Mathematics Department, Computer Science & Information Technology track.
               </p>
             </li>
-            <li class="mb-10 ml-6">
-              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                <svg
-                  aria-hidden="true"
-                  class="w-3 h-3 text-blue-800 dark:text-blue-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+            <li class="mb-16 ml-8">
+              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-[#fff] hover:bg-[#0000A6] transition-colors">
               </span>
-              <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Flowbite Figma v1.3.0
+              <h3 class="mb-1 text-lg font-semibold text-gray-900 ">
+                Graduation Project
               </h3>
               <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                Released on December 7th, 2021
+                2021
               </time>
               <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                All of the pages and components are first designed in Figma and
-                we keep a parity between the two versions even as we update the
-                project.
+              Web application that was focused on helping clients to understand lab tests more and recording for them these test to have a medical history, and some common Q&A.
               </p>
             </li>
-            <li class="ml-6">
-              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                <svg
-                  aria-hidden="true"
-                  class="w-3 h-3 text-blue-800 dark:text-blue-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
+            <li class="ml-8 mb-5">
+              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-[#fff] hover:bg-[#0000A6] transition-colors">
               </span>
-              <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                Flowbite Library v1.2.2
+              <h3 class="mb-1 text-lg font-semibold text-gray-900 ">
+                Information Technology Institute
               </h3>
               <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                Released on December 2nd, 2021
+                2022
               </time>
               <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-                Get started with dozens of web components and interactive
-                elements built on top of Tailwind CSS.
+                Web & User Developer.
               </p>
+            </li>
+            <li class="ml-8 mt-20">
+              <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 -bottom-5 ring-8 ring-white dark:ring-gray-900 dark:bg-[#fff] hover:bg-[#0000A6] transition-colors">
+              </span>
+              <h3 class="mb-1 text-lg font-semibold text-gray-900 absolute -bottom-8">
+                Present
+              </h3>
             </li>
           </ol>
         </div>
