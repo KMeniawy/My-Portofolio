af347fec6e6a371c9c61f86f1ab2785dd3bcc51d
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-04-26 20:46:25 +0200

    contact animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,17 +4,17 @@ const Contact = () => {
   return (
     <>
       <div className="h-[50vh] bg-[#0000A6] flex flex-col justify-center items-center gap-10 ">
-        <h3 className="text-5xl text-white">Hire me</h3>
-        <p className="text-white text-lg ">
+        <h3 className="text-5xl text-white" data-aos="fade-up" data-aos-delay="300">Hire me</h3>
+        <p className="text-white text-lg " data-aos="fade-up" data-aos-delay="300">
           {paragraph}
         </p>
-        <button className="btn bg-white text-[#0000A6] hover:text-white mb-20">
+        <button className="btn bg-white text-[#0000A6] hover:text-white mb-20 border-none" data-aos="fade" data-aos-delay="800">
           Contact Us
         </button>
       </div>
       <div className=" grid grid-cols-2 h-[85vh]">
-        <div className="col-span-1  h-[100%] bg-cover bg-no-repeat" style={{backgroundImage:`url(${sideImg})`}}></div>
-        <div className="">
+        <div className="col-span-1  h-[100%] bg-cover bg-no-repeat" style={{backgroundImage:`url(${sideImg})`}} data-aos="fade-right" data-aos-delay="300"></div>
+        <div data-aos="fade-left" data-aos-delay="300">
           <h2 className="pt-10 text-3xl pl-10 mb-10 ">Contact</h2>
           <form action="#" className="pl-16 flex flex-col gap-8 col-span-2">
             <div className="row form-group">
